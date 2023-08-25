6dde90ca04b19398a8bc8ace69cc58df94bf6dab
Knifer <yeded73422@invodua.com>
2023-08-25 15:30:37 +0300

    worked version 0.2

--- src/FifteenPuzzle/components/FifteenPuzzle.jsx
+++ src/FifteenPuzzle/components/FifteenPuzzle.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { moveTile, shufflePuzzle } from "../../store/redusers/puzzleReducer";
 import "./FifteenPuzzle.css";
--- src/FifteenPuzzle/components/gridSizeSelectInput/GridSizeSelectInput.jsx
+++ src/FifteenPuzzle/components/gridSizeSelectInput/GridSizeSelectInput.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { gridSizeChange } from "../../../store/redusers/puzzleReducer";
 import "./GridSizeSelectInput.css";
