74add371cc15b27f6e80234c5e8fcf92140c59a3
Knifer <yeded73422@invodua.com>
2023-08-25 14:29:37 +0300

    worked version 0.2

--- src/FifteenPuzzle/components/FifteenPuzzle.jsx
+++ src/FifteenPuzzle/components/FifteenPuzzle.jsx
@@ -19,26 +19,11 @@ const FifteenPuzzle = () => {
     OnClickShufflePuzzle();
   }, [gridSize]);
 
-  const isValidMove = (clickedIndex, emptyIndex) => {
-    // Размер сетки (количество рядов или столбцов).
-    const gridSize = Math.sqrt(puzzle.length);
-    // Координаты (ряд и столбец) выбранной и пустой плитки.
-    const clickedRow = Math.floor(clickedIndex / gridSize);
-    const clickedCol = clickedIndex % gridSize;
-    const emptyRow = Math.floor(emptyIndex / gridSize);
-    const emptyCol = emptyIndex % gridSize;
-    // Проверка, смежны ли плитки (разница по горизонтали или вертикали равна 1).
-    const rowDiff = Math.abs(clickedRow - emptyRow);
-    const colDiff = Math.abs(clickedCol - emptyCol);
-    // Проверка, что разница по горизонтали или вертикали равна 1.
-    return (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1);
-  };
-
   const HandleClick = (clickedIndex) => {
     const emptyIndex = puzzle.indexOf(0);
-    if (isValidMove(clickedIndex, emptyIndex)) {
+    if (isValidMove(clickedIndex, emptyIndex, puzzle)) {
       dispatch(moveTile(clickedIndex, emptyIndex));
-      if (isPuzzleSolved()) {
+      if (isPuzzleSolved(realGridSize, puzzle)) {
         alert("Поздравляем! Пазл правильно собран!");
       }
     }
@@ -46,25 +31,6 @@ const FifteenPuzzle = () => {
 
   const OnClickShufflePuzzle = () => {
     dispatch(shufflePuzzle());
-  };
-
-  const isPuzzleSolved = () => {
-    // Правильная последовательность чисел от 1 до 15.
-    const correctSequence = Array.from(
-      { length: realGridSize - 1 },
-      (_, i) => i + 1
-    );
-    // Проверяем, совпадает ли текущая последовательность с правильной.
-    for (let i = 0; i < 15; i++) {
-      if (puzzle[i] !== correctSequence[i]) {
-        return false;
-      }
-    }
-    // Проверяем, что последняя плитка пуста (с числом 0).
-    if (puzzle[15] !== 0) {
-      return false;
-    }
-    return true; // Если все условия выполнены, пазл правильно собран.
   };
 
   const stylePuzzleContainer = getPuzzleContainerStule(gridSize);
@@ -103,3 +69,37 @@ const getPuzzleContainerStule = (gridSize) => {
 };
 
 export default FifteenPuzzle;
+
+const isValidMove = (clickedIndex, emptyIndex, puzzle) => {
+  // Размер сетки (количество рядов или столбцов).
+  const gridSize = Math.sqrt(puzzle.length);
+  // Координаты (ряд и столбец) выбранной и пустой плитки.
+  const clickedRow = Math.floor(clickedIndex / gridSize);
+  const clickedCol = clickedIndex % gridSize;
+  const emptyRow = Math.floor(emptyIndex / gridSize);
+  const emptyCol = emptyIndex % gridSize;
+  // Проверка, смежны ли плитки (разница по горизонтали или вертикали равна 1).
+  const rowDiff = Math.abs(clickedRow - emptyRow);
+  const colDiff = Math.abs(clickedCol - emptyCol);
+  // Проверка, что разница по горизонтали или вертикали равна 1.
+  return (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1);
+};
+
+const isPuzzleSolved = (realGridSize, puzzle) => {
+  // Правильная последовательность чисел от 1 до 15.
+  const correctSequence = Array.from(
+    { length: realGridSize - 1 },
+    (_, i) => i + 1
+  );
+  // Проверяем, совпадает ли текущая последовательность с правильной.
+  for (let i = 0; i < 15; i++) {
+    if (puzzle[i] !== correctSequence[i]) {
+      return false;
+    }
+  }
+  // Проверяем, что последняя плитка пуста (с числом 0).
+  if (puzzle[15] !== 0) {
+    return false;
+  }
+  return true; // Если все условия выполнены, пазл правильно собран.
+};
